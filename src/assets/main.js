4ab655f2d4ff47602861223023b7377fe4764397
handeger <h.degeringel@gmail.com>
2017-02-13 15:03:16 +0100

    Add files via upload

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,37 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/handeger.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      populateWithCourses(data.courses.completed)
+    }
+  });
 
+  function populateWithCourses(courses) {
+    var $badges = $('#badges')
+
+    courses.foreach(function(cource) {
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+    });
+  }
 });
